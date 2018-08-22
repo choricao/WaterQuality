1c9f513edcdc7bddc6fd7bbec6391eeccbc42419
Zheng Cao <zheng.cao@milliman.com>
2018-08-22 16:37:42 -0700

    Identify and remove outliers

--- dataWrangling.R
+++ dataWrangling.R
@@ -56,6 +56,47 @@ glimpse(subset(filtered_water, unit == 'MG/L' & parameter == "PH"))
 convert <- which(filtered_water$unit == 'MG/L' & filtered_water$parameter == "PH")
 filtered_water$unit[convert] <- 'Standard units'
 
+glimpse(subset(filtered_water, unit == 'MG/L'))
 
+glimpse(subset(filtered_water, unit == 'MG/L' & result > 70))
 
+convert <- which(filtered_water$unit == 'MG/L' & filtered_water$result > 70)
+filtered_water$unit[convert] <- 'Deg. Fahrenheit'
+
+glimpse(subset(filtered_water, unit == 'MG/L'))
+
+convert <- which(filtered_water$unit == 'MG/L')
+filtered_water$unit[convert] <- 'Deg. Celsius'
+
+summary(filtered_water)
+
+# Identify and remove outliers
+ggplot(filtered_water, mapping = aes(x = sampleTime, y = result)) +
+  geom_point()
+
+subset(filtered_water, result > 1000000) %>%
+  glimpse()
+
+remove <- which(filtered_water$result > 1000000 | is.na(filtered_water$result))
+filtered_water <- filtered_water[-remove, ]
+
+summary(filtered_water)
+
+subset(filtered_water, result > 1000) %>%
+  glimpse()
+
+remove <- which(filtered_water$result > 1000)
+filtered_water <- filtered_water[-remove, ]
+
+summary(filtered_water)
+
+ggplot(data = filtered_water, mapping = aes(x = unit, y = result)) +
+  geom_boxplot()
+
+convert <- which(filtered_water$result > 60 &
+                   filtered_water$unit == "Deg. Celsius")
+filtered_water$unit[convert] <- 'Deg. Fahrenheit'
+
+ggplot(data = filtered_water, mapping = aes(x = unit, y = result)) +
+  geom_boxplot()
 
